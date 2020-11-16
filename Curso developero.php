89748b13b7b0412271d4a9a52f5de4f24d1028e0
FrannM <fmaurizzio@gmail.com>
2020-11-15 21:54:06 -0300

    Ok

---
+++ Curso developero.php
@@ -0,0 +1,33 @@
+<?php
+  //Imprimir en pantalla
+  echo "esto es una cadena </br>";
+
+  print ("esto es una cadena impresa por print<br>");
+
+  //Variables = espacio reservado en el disco odbc_procedurecolumns
+
+  $n = 1;//enter0
+  $altura = 1.6;//flotante
+
+  //String
+  $str = "Esto es una cadena";
+  $str2 = "Esto es otra cadena";
+
+  echo "$str<br>";
+  echo "$str2<br>";
+
+  //boolean sólo dos valores posibles
+  $bool =false;
+  $bool2=true;
+
+  //array
+  $datos = array('nombre' => "Franco",
+				 'edad' => "25",
+				 'Localidad' => "Buenos Aires");
+
+  $paises = array('Argentina', 'Brasil', 'Mexico', 'Colombia');
+
+print_r($paises);
+echo "<br>";
+ print_r($datos);
+?>
