680c9ceea9fb723e7bfa23805b565cab58f7e898
FrannM <fmaurizzio@gmail.com>
2020-11-15 22:37:20 -0300

    Init Commit

--- Curso developero.php
+++ Curso developero.php
@@ -1,34 +1,24 @@
 <?php
-  //Imprimir en pantalla
-  echo "esto es una cadena </br>";
-
-  print ("esto es una cadena impresa por print<br>");
-
-  //Variables = espacio reservado en el disco
-
-  $n = 1;//enter0
-  $altura = 1.6;//flotante
-
-  //String
-  $str = "Esto es una cadena";
-  $str2 = "Esto es otra cadena";
-
-  echo "$str<br>";
-  echo "$str2<br>";
-
-  //boolean sólo dos valores posibles
-  $bool =false;
-  $bool2=true;
-
-  //array
-  $datos = array('nombre' => "Franco",
-                  'edad' => "25",
-				          'Localidad' => "Buenos Aires");
-
-  $paises = array('Argentina', 'Brasil', 'Mexico', 'Colombia');
-
-
-  print_r($paises);
-  echo "<br>";
-  print_r($datos);
+	//Imprimir en pantalla
+	echo "esto es una cadena </br>";
+	print ("esto es una cadena impresa por print<br>");
+	//Variables = espacio reservado en el disco
+	$n = 1;//enter0
+	$altura = 1.6;//flotante
+	//String
+	$str = "Esto es una cadena";
+	$str2 = "Esto es otra cadena";
+	echo "$str<br>";
+	echo "$str2<br>";
+	//boolean sólo dos valores posibles
+	$bool =false;
+	$bool2=true;
+	//array
+	$datos = array('nombre' => "Franco",
+					'edad' => "25",
+					'Localidad' => "Buenos Aires");
+	$paises = array('Argentina', 'Brasil', 'Mexico', 'Colombia');
+	print_r($paises);
+	echo "<br>";
+	print_r($datos);
 ?>
